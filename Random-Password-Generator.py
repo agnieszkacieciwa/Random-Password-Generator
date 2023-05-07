941027e5554141788f7db2a85d38449bbface2ba
agnieszkacieciwa <88035266+agnieszkacieciwa@users.noreply.github.com>
2023-05-07 12:07:18 +0100

    Update Random-Password-Generator.py

--- Random-Password-Generator.py
+++ Random-Password-Generator.py
@@ -1,17 +1,17 @@
 import random
 import string
 
-# Lista znaków, z których ma składać się hasło
+# List of characters to compose the password
 chars = string.ascii_letters + string.digits + string.punctuation
 
-# Funkcja generująca hasło o zadanej długości
+# Function that generates a password of a given length
 def generate_password(length):
-    # Losujemy z listy `chars` `length` razy
+    # Choose from the `chars` list `length` times
     password = ''.join(random.choice(chars) for i in range(length))
     return password
 
-# Wywołujemy funkcję `generate_password` z argumentem `length` równym 12 (długość hasła)
+# Call the `generate_password` function with the `length` argument set to 12
 password = generate_password(12)
 
-# Wyświetlamy wygenerowane hasło
-print("Wygenerowane hasło to:", password)
+# Display the generated password
+print("Generated password:", password)
